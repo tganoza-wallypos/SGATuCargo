62ab6f050d0e7a4494b24822f59a126472426453
Sayuricornejo <150866711+Sayuricornejo@users.noreply.github.com>
2023-11-15 19:52:26 -0500

    Update almacenero.php

--- pages/almacenero.php
+++ pages/almacenero.php
@@ -5,16 +5,13 @@ include'../includes/';
 $product_ids = array();
 //session_destroy();
 
-//check if Add to Cart button has been submitted
 if(filter_input(INPUT_POST, 'addpos')){
     if(isset($_SESSION['pointofsale'])){
         
         //keep track of how mnay products are in the shopping cart
         $count = count($_SESSION['pointofsale']);
-        
         //create sequantial array for matching array keys to products id's
         $product_ids = array_column($_SESSION['pointofsale'], 'id');
-
         if (!in_array(filter_input(INPUT_GET, 'id'), $product_ids)){
         $_SESSION['pointofsale'][$count] = array
             (
@@ -32,8 +29,7 @@ if(filter_input(INPUT_POST, 'addpos')){
                     $_SESSION['pointofsale'][$i]['quantity'] += filter_input(INPUT_POST, 'quantity');
                 }
             }
-        }
-        
+        } 
     }
     else { //if shopping cart doesn't exist, create first product with array key 0
         //create array using submitted form data, start from key 0 and fill it with values
@@ -46,7 +42,6 @@ if(filter_input(INPUT_POST, 'addpos')){
         );
     }
 }
-
 if(filter_input(INPUT_GET, 'action') == 'delete'){
     //loop through all products in the shopping cart until it matches with GET id variable
     foreach($_SESSION['pointofsale'] as $key => $product){
@@ -58,9 +53,7 @@ if(filter_input(INPUT_GET, 'action') == 'delete'){
     //reset session array keys so they match with $product_ids numeric array
     $_SESSION['pointofsale'] = array_values($_SESSION['pointofsale']);
 }
-
 //pre_r($_SESSION);
-
 function pre_r($array){
     echo '<pre>';
     print_r($array);
@@ -85,23 +78,19 @@ function pre_r($array){
 <!-- TAB PANE AREA - ANG UNOD KA TABS ARA SA TABPANE.PHP -->
 <?php include 'postabpane.php'; ?>
 <!-- END TAB PANE AREA - ANG UNOD KA TABS ARA SA TABPANE.PHP -->
-
         <div style="clear:both"></div>  
         <br />  
         <div class="card shadow mb-4 col-md-12">
         <div class="card-header py-3 bg-white">
           <h4 class="m-2 font-weight-bold text-primary">Punto de registro de productos en los lotes:</h4>
         </div>
-        
       <div class="row">    
       <div class="card-body col-md-9">
         <div class="table-responsive">
-
         <!-- trial form lang   -->
 <form role="form" method="post" action="pos_transac.php?action=add">
   <input type="hidden" name="employee" value="<?php echo $_SESSION['FIRST_NAME']; ?>">
   <input type="hidden" name="role" value="<?php echo $_SESSION['JOB_TITLE']; ?>">
-  
         <table class="table">    
         <tr>  
              <th width="55%">Nombre del producto</th>  
@@ -113,9 +102,7 @@ function pre_r($array){
         <?php  
 
         if(!empty($_SESSION['pointofsale'])):  
-            
              $total = 0;  
-        
              foreach($_SESSION['pointofsale'] as $key => $product): 
         ?>  
         <tr>  
@@ -128,7 +115,6 @@ function pre_r($array){
             <input type="hidden" name="quantity[]" value="<?php echo $product['quantity']; ?>">
             <?php echo $product['quantity']; ?>
           </td>  
-
            <td>
             <input type="hidden" name="price[]" value="<?php echo $product['price']; ?>">
             ₱ <?php echo number_format($product['price']); ?>
@@ -144,7 +130,7 @@ function pre_r($array){
            </td>  
         </tr>
         <?php  
-                  $total = $total + ($product['quantity'] * $product['price']);
+             $total = $total + ($product['quantity'] * $product['price']);
              endforeach;  
         ?>
         <?php  
@@ -153,7 +139,6 @@ function pre_r($array){
         </table> 
          </div>
        </div> 
-
 <?php
 include 'posside.php';
 include'../includes/footer.php';
